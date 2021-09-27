906b3f18bb78b56fbeccff06b596e85c48c36970
freelancernoor <info@devnurul.me>
2021-09-27 17:39:42 +0600

    I am changin all think witg react.js

---
+++ src/component/Shop/Shop.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import PorductsData from '../../fakeData/products.JSON';
+
+const Shop = () => {
+    useState()
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Shop;
