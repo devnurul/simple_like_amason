8f42aedeb2605ac469e85fdc5cf07b56ee551635
freelancernoor <info@devnurul.me>
2021-09-30 02:02:20 +0600

    Hello js

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -3,8 +3,7 @@ import React from 'react';
 const Cart = (props) => {
     return (
         <div>
-             <h4>This is cart</h4> 
-                <h4>Oreder Summery: {props.cart.length}</h4>
+               
         </div>
     );
 };
